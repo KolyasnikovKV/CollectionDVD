8cd4715e941ed08739f022d1e7f767bfe542e094
Kirill <1c81@mail.ru>
2020-01-12 13:12:16 +0300

    Добавил JpaRepository

---
+++ src/main/java/ru/kolyasnikovkv/collectiondvd/generics/CommonRepository.java
@@ -0,0 +1,9 @@
+package ru.kolyasnikovkv.collectiondvd.generics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface CommonRepository<E extends AbstractEntity> extends JpaRepository<E, Long> {
+}
