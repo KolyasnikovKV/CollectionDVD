14ac15a78abc0dc060417bb79a3aac44b9608b81
Kirill <1c81@mail.ru>
2020-01-12 12:55:53 +0300

    Добавил AbstractEntity

---
+++ src/main/java/ru/kolyasnikovkv/collectiondvd/generics/AbstractEntity.java
@@ -0,0 +1,23 @@
+package ru.kolyasnikovkv.collectiondvd.generics;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+
+    private Long id;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
