78e56e77893c9fa294d6f43c51635084dff69f0b
Kirill <1c81@mail.ru>
2020-01-16 00:17:22 +0300

    Добавил JdbcCountryRepository

---
+++ src/main/java/ru/kolyasnikovkv/collectiondvd/repository/jdbc/JdbcCountryRepository.java
@@ -0,0 +1,32 @@
+package ru.kolyasnikovkv.collectiondvd.repository.jdbc;
+
+import org.springframework.stereotype.Repository;
+import ru.kolyasnikovkv.collectiondvd.model.Country;
+import ru.kolyasnikovkv.collectiondvd.model.Dvd;
+import ru.kolyasnikovkv.collectiondvd.repository.CountryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Repository
+public class JdbcCountryRepository implements CountryRepository {
+
+    public Country save(Country country) {
+        return country;
+    }
+
+    public void delete(Long id) {
+    }
+
+    public Country get(Long id) {
+        return new Country();
+    }
+
+    public List<Country> findAll() {
+        return new ArrayList<Country>();
+    }
+
+    public List<Country> findByName(String name) {
+        return new ArrayList<Country>();
+    }
+}
