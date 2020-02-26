69255d1be72f590fbaef4f8148727ddf73264e5d
Kirill <1c81@mail.ru>
2020-02-26 13:43:53 +0300

    Добавил Connection Poll через dbcp2

--- src/main/java/ru/kolyasnikovkv/collectiondvd/configuration/DataProvider.java
+++ src/main/java/ru/kolyasnikovkv/collectiondvd/configuration/DataProvider.java
@@ -1,10 +1,14 @@
 package ru.kolyasnikovkv.collectiondvd.configuration;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -15,6 +19,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan({"ru.kolyasnikovkv.collectiondvd.repository.datajpa",
@@ -26,27 +31,55 @@ import javax.sql.DataSource;
 @PropertySource("classpath:/postgres.properties")
 public class DataProvider {
 
+    /*
+    КАк лучше?
+    @Autowired
+    private Environment environment;
+    environment.getRequiredProperty("db.driver")
+    dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+    */
+
    @Bean
     public DataSource dataSource(
+            @Value("${database.driver}") String driver,
             @Value("${database.url}") String url,
             @Value("${database.password}") String pwd,
-            @Value("${database.username}") String user) {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
+            @Value("${database.username}") String user,
+            @Value("${database.pool.initSize}") String initSize,
+            @Value("${database.pool.maxSize}") String maxSize) {
+       /* DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setUrl(url);
         ds.setUsername(user);
         ds.setPassword(pwd);
-        return ds;
+        return ds;*/
+       BasicDataSource dataSource = new BasicDataSource();
+       dataSource.setDriverClassName(driver);
+       dataSource.setUrl(url);
+       dataSource.setUsername(user);
+       dataSource.setPassword(pwd);
+       dataSource.setInitialSize(Integer.parseInt(initSize));
+       dataSource.setMaxTotal(Integer.parseInt(maxSize));
+       return dataSource;
     }
 
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        //properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("javax.persistence.validation.mode", "none");
+        properties.put("org.hibernate.flushMode", "COMMIT");
+        return properties;
+    }
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
-        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-        lef.setDataSource(dataSource);
-        lef.setJpaVendorAdapter(jpaVendorAdapter);
-        lef.setPackagesToScan("ru.kolyasnikovkv.collectiondvd");
-        return lef;
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactory.setPackagesToScan("ru.kolyasnikovkv.collectiondvd");
+        entityManagerFactory.setJpaProperties(hibernateProperties());
+        return entityManagerFactory;
     }
 
     @Bean
@@ -57,8 +90,17 @@ public class DataProvider {
         hibernateJpaVendorAdapter.setDatabase(Database.POSTGRESQL);
         return hibernateJpaVendorAdapter;
     }
+
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new JpaTransactionManager();
     }
+
+    /*@Bean
+    public JpaTransactionManager transactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }*/
+
 }
--- src/main/java/ru/kolyasnikovkv/collectiondvd/configurationJPA/ServiceConfig_.java
+++ src/main/java/ru/kolyasnikovkv/collectiondvd/configurationJPA/ServiceConfig_.java
@@ -1,16 +1,29 @@
 package ru.kolyasnikovkv.collectiondvd.configurationJPA;
 
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
+/**
+ * 
+ *//*
 @Configuration
-@ComponentScan({ "ru.kolyasnikovkv.collectiondvd" })
+@ComponentScan({ "ru.kolyasnikovkv.collectiondvd.service", "net.devstudy.resume.component.impl" })
+@EnableAspectJAutoProxy
 public class ServiceConfig {
+	
+	@Bean
+	public PropertiesFactoryBean properties(){
+		PropertiesFactoryBean properties = new PropertiesFactoryBean();
+		properties.setLocations(new ClassPathResource("logic.properties"));
+		return properties;
+	}
 	
 	/**
 	 * http://docs.spring.io/autorepo/docs/spring/4.2.5.RELEASE/spring-framework-reference/html/beans.html
@@ -19,10 +32,11 @@ public class ServiceConfig {
 	 * Those should usually be declared as static @Bean methods, not triggering the instantiation of their containing configuration class. 
 	 * Otherwise, @Autowired and @Value won't work on the configuration class itself since it is being created as a bean instance too early.
 	 */
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() throws IOException{
+/*	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() throws IOException {
 		PropertySourcesPlaceholderConfigurer conf = new PropertySourcesPlaceholderConfigurer();
-		conf.setLocations(new ClassPathResource("application.properties"));
+		conf.setLocations(new ClassPathResource("logic.properties"), new ClassPathResource("properties/application.properties"), new ClassPathResource("properties/elasticsearch.properties"));
 		return conf;
 	}
 }
+*/
